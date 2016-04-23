e4d86c7907d05be0493187673c22f4891d22bc43
Shenli Lund <shenlilund@gmail.com>
2016-04-23 16:37:53 -0500

    Added the business logic class, as well as the class variables for it.

---
+++ CMSC495_Capstone/BusinessLogic.java
@@ -0,0 +1,27 @@
+// CMSC 495
+// Shenli Lund
+// April 23, 2016
+
+public class BusinessLogic
+{
+  private int customer; //TODO Why are these ints? Shouldn't they be objects? like private Customer customer;
+  private int room;
+  private int customerID;
+  private int roomNumber;
+  private int numberOfOccupants;
+  private Date checkInDate;
+  private Date checkOutDate;
+  private int reservationID;
+  private int roomNumber;
+  private Type roomType;   //TODO we don't have a Type class?
+  private String firstName; //TODO if we have a customer object, we could get the names from that
+  private String lastName;
+  private String address;
+
+
+  public int createReservation()
+  {
+    
+  }
+
+}
